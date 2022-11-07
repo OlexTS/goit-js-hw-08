4334884c0f4284bf29ed3cfff657d2ce0577775b
OlexTS <sashokseven@gmail.com>
2022-11-07 19:16:56 +0200

    finished task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const playerRef = document.querySelector('#vimeo-player');
+const playerTime = 'videoplayer-current-time';
+
+const player = new Player(playerRef);
+function onChangeTime(data) {
+  localStorage.setItem(
+    playerTime,
+    JSON.stringify(data.seconds)
+  );
+}
+player.on('timeupdate', throttle(onChangeTime, 1000));
+if(localStorage.getItem(
+   playerTime)) {
+   player.setCurrentTime(localStorage.getItem(playerTime));
+    }
 
