318f1af921d703574638b4bdc679adb19d656126
OlexTS <sashokseven@gmail.com>
2022-11-08 00:41:27 +0200

    finish task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const playerRef = document.querySelector('#vimeo-player');
-const playerTime = 'videoplayer-current-time';
+const PLAYERTIME_KEY = 'videoplayer-current-time';
 
 const player = new Player(playerRef);
 function onChangeTime(data) {
   localStorage.setItem(
-    playerTime,
+    PLAYERTIME_KEY,
     JSON.stringify(data.seconds)
   );
 }
 player.on('timeupdate', throttle(onChangeTime, 1000));
 if(localStorage.getItem(
-   playerTime)) {
-   player.setCurrentTime(localStorage.getItem(playerTime));
+   PLAYERTIME_KEY)) {
+   player.setCurrentTime(localStorage.getItem(PLAYERTIME_KEY));
     }
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(onChangeInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+const LOCALESTORAGE_KEY = 'feedback-form-state';
+const inputValue = {};
+
+function onChangeInput(event) {
+  if (event.target.nodeName === 'INPUT') {
+    inputValue.email = event.target.value;
+  } else if (event.target.nodeName === 'TEXTAREA') {
+    inputValue.message = event.target.value;
+  }
+
+  localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(inputValue));
+}
+
+const parsedVal = JSON.parse(localStorage.getItem(LOCALESTORAGE_KEY));
+if (parsedVal) {
+  formRef.elements.email.value = parsedVal.email || '';
+  formRef.elements.message.value = parsedVal.message || '';
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(inputValue);
+  localStorage.removeItem(LOCALESTORAGE_KEY);
+  formRef.reset();
+}
